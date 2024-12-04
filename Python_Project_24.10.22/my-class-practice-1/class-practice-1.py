4e7ef0c7895e2543d3323b5a85211450c0cd9405
YuyanSH <2735389670@qq.com>
2024-12-04 18:08:22 +0800

    新作了python笔记

--- Python_Project_24.10.22/my-class-practice-1/class-practice-1.py
+++ Python_Project_24.10.22/my-class-practice-1/class-practice-1.py
@@ -1,3 +1,4 @@
+"""
 import math
 
 class Circle:
@@ -7,13 +8,25 @@ class Circle:
     def calculate_area(self):
         return math.pi * self.radius ** 2
     
-from circle import Circle
+#from circle import Circle
 
 circle_1 = Circle(42)
 circle_2 = Circle(7)
-
-circle_1
-
-circle_2
-
+#circle_1.radius = 100
 print(circle_1.calculate_area())
+"""
+
+"""
+#__value在外部其实是_SampleClass__value，即在内部变量名称前面加__会使该变量在外部的名称前面加上_类名称
+class SampleClass:
+    def __init__(self, value):
+        self.__value = value
+    def __method(self):
+        print(self.__value)
+
+sample_instance = SampleClass("hello")
+sample_instance._SampleClass__method()
+print(vars(sample_instance))
+"""
+
+
--- Python_Project_24.10.22/my-class-practice-1/class-test-1.py
+++ Python_Project_24.10.22/my-class-practice-1/class-test-1.py
@@ -1,10 +1,5 @@
-from circle import Circle
+from car import Car
 
-circle_1 = Circle(42)
-circle_2 = Circle(7)
-
-circle_1
-
-circle_2
-
-print(circle_1.calculate_area())
+toyota_camry = Car("Toyota", "Camry", 2022, "Red")
+print(toyota_camry.year)
+print(toyota_camry.color)
